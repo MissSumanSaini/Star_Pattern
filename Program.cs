f4c6285bbb31ada9b9e2b5535dc00f16a0e2d3ce
suman <suman@SUMAN>
2022-04-16 20:29:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Star_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for(i=1; i<=7; i++) // 1  2 
+            {
+                for(j=1; j<=i; j++) //1 12
+                {
+                    Console.Write("*");  //* **
+                }
+                Console.WriteLine(" ");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
